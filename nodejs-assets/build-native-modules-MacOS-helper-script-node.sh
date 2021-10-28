cb99dca71cd06e0e51043d3a54c4c3af7a374f33
Jose Marmolejos <josemarmolejos@xennialconsulting.com>
2021-10-28 11:10:05 -0400

    add icon

---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call node on macOS in case it is not found
+      export PATH=$PATH:/Users/jose/.nvm/versions/node/v14.16.1/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/jose/Desktop/workspace/myproyects/instamint/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/jose/Desktop/workspace/myproyects/instamint/node_modules/.bin:/Users/jose/.nvm/versions/node/v14.16.1/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/Users/jose/Library/Android/sdk/emulator:/Users/jose/Library/Android/sdk/tools:/Users/jose/Library/Android/sdk/tools/bin:/Users/jose/Library/Android/sdk/platform-tools
+      node $@
+    
